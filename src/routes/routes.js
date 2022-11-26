693399c15cf8d3eb7a9dbbfee3cf9b7a79b750fb
Andrey <androrvit@gmail.com>
2022-11-26 11:45:50 +0300

    create TreePage

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,15 @@
-import {ADMIN, AUTH_PAGE, MAIN_PAGE} from "../util/consts";
+import { ADMIN, AUTH_PAGE, MAIN_PAGE, TREE_PAGE } from "../util/consts";
 import MainPage from "../pages/MainPage";
 import Admin from "../pages/Admin";
 import { Navigate } from "react-router-dom";
 import AuthPage from "../pages/AuthPage";
+import TreePage from "../pages/TreePage";
 
 export const authRoutes = [{ path: ADMIN, component: <Admin />, exact: true }];
 
 export const publicRoutes = [
     { path: MAIN_PAGE, component: <MainPage />, exact: true },
     { path: AUTH_PAGE, component: <AuthPage />, exact: true },
+    { path: TREE_PAGE, component: <TreePage />, exact: true },
     { path: "*", component: <Navigate to={MAIN_PAGE} replace />, exact: true },
 ];
--- src/util/consts.js
+++ src/util/consts.js
@@ -1,3 +1,4 @@
 export const MAIN_PAGE = "/";
 export const AUTH_PAGE = "/auth";
+export const TREE_PAGE = "/tree";
 export const ADMIN = "/admin";
